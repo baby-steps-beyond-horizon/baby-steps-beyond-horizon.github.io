ff9599730309277c8b58a9816980b27cc1793042
dupa <weronikajakimowicz2@gmail.com>
2024-09-05 10:39:26 +0200

    programme_poster

--- src/scripts/generate_programme.py
+++ src/scripts/generate_programme.py
@@ -1,7 +1,7 @@
 import json
 from jinja2 import Environment, FileSystemLoader
 
-f = open('/home/marudka/baby-steps-beyond-horizon.github.io/data/programme.json')
+f = open('data/programme.json')
 txt = json.load(f)
 
 environment = Environment(loader=FileSystemLoader("template/"))
@@ -12,3 +12,49 @@ cnt = template.render(days=txt)
 with open("programme.html", mode="w", encoding="utf-8") as m:
     m.write(cnt)
 
+f.close()
+
+dates_file = open('data/important_dates.json')
+dates_json = json.load(dates_file)
+template = environment.get_template("index.html")
+
+dic = []
+i = 0
+
+for d in dates_json:
+    bad_date = d['date']
+    nice_date = str(bad_date[2]) + "." + str(bad_date[1]+1) + "." + str(bad_date[0])
+    i += 1
+
+    new_id = "c" + str(i)
+
+    dic.append({
+        "day" : nice_date, 
+        "id" : new_id,
+        "description" : d["description"]
+        })
+
+cnt = template.render(days = dic)
+
+with open("src/scripts/calendar.js") as f:
+    lines = f.readlines()
+
+new_line = """const interesting_dates = ["""
+                                         #new Date(2024, 7, 23), new Date(2024, 7, 28), new Date(2024, 8, 1), new Date(2024, 8, 1), new Date(2024, 8, 3)];
+i = 0
+for d in dates_json:
+    day = d['date']
+    i += 1
+    new_line += f"new Date({day[0]}, {day[1]}, {day[2]})"
+    if i < len(dates_json):
+        new_line += ", "
+new_line += "];\n"
+
+lines[0] = new_line
+
+
+with open("src/scripts/calendar.js", "w") as f:
+    f.writelines(lines)
+
+with open("index.html", mode="w", encoding="utf-8") as m:
+    m.write(cnt)
--- src/scripts/menu_handler.js
+++ src/scripts/menu_handler.js
@@ -1,116 +1,52 @@
-window.addEventListener('load', function(event) {
+function handle_buttons() {
   btn_holder = document.getElementById('btn-holder');
   drop = document.getElementById('dropdown');
   drop_ctn = document.getElementById('dropdown-content');
 
   holder_width = btn_holder.offsetWidth;
 
-  btn_lst = [];
-  drop_lst = [];
+  btn_widths = [80, 90, 140, 135, 115, 140, 130];
+  more_width = 140;
+  btn_lst = []
+  drop_lst = []
 
-  btn_width = 0;
+  btn_w = 0
+  dropped = false;
+  
   for (let i = 0; i < 7; i++) {
-    next_btn = document.getElementById(i.toString());
-    if (next_btn.parentNode.id == 'btn-holder') {
-      btn_width += next_btn.offsetWidth;
-      btn_lst.push(next_btn);
+    if (btn_w + btn_widths[i] + 30 < holder_width) {
+      btn_lst.push(document.getElementById(i.toString()));
+      btn_w += btn_widths[i] + 30;
     }
     else {
-      drop_lst.push(next_btn);
+      let j = i - 1;
+      while (btn_w + 30 >= holder_width && btn_lst.length > 0) {
+        bb = btn_lst.pop();
+        drop_lst.push(bb);
+        if (!dropped) {
+          btn_w + more_width + 30;
+          dropped = true;
+        }
+        btn_w -= btn_widths[j];
+      }
+      drop_lst.push(document.getElementById(i.toString()));
     }
   }
-
-  drop_lst.reverse();
-
-  // while ((drop_lst.length > 0 && drop_lst[drop_lst.length - 1].offsetWidth + btn_width < holder_width) || 
-  //   (drop_lst.length == 1 && drop_lst[0].offsetWidth + btn_width - 80 < holder_width) ) {
-  //   to_move = drop_lst.pop();
-  //   btn_width += to_move.offsetWidth;
-  //   console.log("usuniete", to_move);
-  //
-  //   btn_holder.append(to_move);
-  //   btn_holder.append(drop);
-  // }
-
-
-  add_more = true;
-  if (drop.style.display != 'none') {
-    add_more = false;
-    btn_width += document.getElementById('more').offsetWidth;
+  
+  for (let i = 0; i < btn_lst.length; i++) {
+    btn_holder.append(btn_lst[i]);
   }
 
-  while (btn_width >= holder_width && btn_lst.length > 0) {
-    if (add_more) {
-      drop.style.display = 'inline-block';
-      btn_width += document.getElementById('more').offsetWidth;
-      add_more = false;
-    }
-    to_move = btn_lst.pop();
-    btn_width -= to_move.offsetWidth;
-    drop_ctn.append(to_move);
-    drop_lst.push(to_move);
+  btn_holder.append(drop);
+
+  if (drop_lst.length > 0) drop.style.display = 'inline-block';
+  else drop.style.display = 'none';
+
+  for (let i = 0; i < drop_lst.length; i++) {
+    drop_ctn.append(drop_lst[i]);
   }
 
-  if (drop_lst.length == 0) drop.style.display = 'none';
-  else drop.style.display = 'inline-block';
+}
 
-  // if (drop_lst.length == 0) drop.style.display = 'none';
-}, true);
-
-window.addEventListener('resize', function(event) {
-  btn_holder = document.getElementById('btn-holder');
-  drop = document.getElementById('dropdown');
-  drop_ctn = document.getElementById('dropdown-content');
-
-  holder_width = btn_holder.offsetWidth;
-
-  btn_lst = [];
-  drop_lst = [];
-
-  btn_width = 0;
-  for (let i = 0; i < 7; i++) {
-    next_btn = document.getElementById(i.toString());
-    if (next_btn.parentNode.id == 'btn-holder') {
-      btn_width += next_btn.offsetWidth;
-      btn_lst.push(next_btn);
-    }
-    else {
-      drop_lst.push(next_btn);
-    }
-  }
-
-  drop_lst.reverse();
-
-  add_more = true;
-  if (drop.style.display != 'none') {
-    add_more = false;
-    btn_width += document.getElementById('more').offsetWidth;
-  }
-
-  while (btn_width >= holder_width && btn_lst.length > 0) {
-    if (add_more) {
-      drop.style.display = 'inline-block';
-      btn_width += document.getElementById('more').offsetWidth;
-      add_more = false;
-    }
-    to_move = btn_lst.pop();
-    btn_width -= to_move.offsetWidth;
-    drop_ctn.append(to_move);
-    drop_lst.push(to_move);
-  }
-
-  to_move = drop_lst[drop_lst.length - 1];
-  while (drop_lst.length > 0 && 180 + btn_width < holder_width) {
-    btn_width += to_move.offsetWidth;
-    btn_holder.append(to_move);
-    btn_holder.append(drop);
-    drop_lst.pop();
-    to_move = drop_lst[drop_lst.length -1];
-  }
-
-  console.log(btn_width, holder_width);
-
-  if (drop_lst.length == 0) drop.style.display = 'none';
-  else drop.style.display = 'inline-block';
-
-}, true);
+window.addEventListener('load', function(event) { handle_buttons(); });
+window.addEventListener('resize', function(event) { handle_buttons(); });
